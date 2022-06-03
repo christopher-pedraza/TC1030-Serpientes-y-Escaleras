e777c9a5b75f5252cc375b015bfaaff5c2ba8d9a
Guillermo Garrido <memocei@hotmail.com>
2022-06-03 10:40:11 -0500

    main conflict

--- Codigo/main.cpp
+++ Codigo/main.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:main.cpp
+
 /****************************************************
 TC1030 - Equipo 8
 SerpientesEscaleras
@@ -15,21 +15,3 @@ int main(int argc, const char * argv[]) {
     g.start(); // inicia la ejecucion del juego
     return 0;
 }
-=======
-/****************************************************
-TC1030 - Equipo 8
-SerpientesEscaleras
-main.cpp
-*****************************************************/
-
-#include "MyGame.h"
-
-// Con este codigo se inicia el juego
-int main(int argc, const char * argv[]) {
-    Board table = Board();
-    table.draw();
-    MyGame g; // se crea una nueva instancia del juego para poder llamarlo
-    g.start(); // inicia la ejecucion del juego
-    return 0;
-}
->>>>>>> 806f80dde73db9610e3b129adbd2ab1ed46adfc9:Codigo/main.cpp
