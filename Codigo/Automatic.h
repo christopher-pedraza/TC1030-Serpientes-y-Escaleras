a766b505c4face65c82bb93b88170b02d727bb7c
Christopher Pedraza Pohlenz <62347713+christopher-pedraza@users.noreply.github.com>
2022-06-03 09:56:21 -0500

    Agregar archivos Automatic.h y Manual.h

---
+++ Codigo/Automatic.h
@@ -0,0 +1,11 @@
+#ifndef Automatic_h
+#define Automatic_h
+
+#include <iostream>
+#include "MyGame.h"
+
+class Automatic: public MyGame {
+    
+};
+
+#endif /* Automatic_h */
---
+++ Codigo/Manual.h
@@ -0,0 +1,11 @@
+#ifndef Manual_h
+#define Manual_h
+
+#include <iostream>
+#include "MyGame.h"
+
+class Manual: public MyGame {
+    
+};
+
+#endif /* Manual_h */
