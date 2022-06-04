f71b5a7b0f7078b4476d0c2a0cd8708a2ebbccf1
Christopher Pedraza Pohlenz <62347713+christopher-pedraza@users.noreply.github.com>
2022-06-03 21:29:04 -0500

    Agregar opciones para parametrización

--- Codigo/main.cpp
+++ Codigo/main.cpp
@@ -7,12 +7,31 @@ main.cpp
 
 #include "MyGame.h"
 #include "Manual.h"
+#include "Automatic.h"
 
 // Con este codigo se inicia el juego
 int main(int argc, const char * argv[]) {
     Board table = Board();
     table.draw();
-    Manual g; // se crea una nueva instancia del juego para poder llamarlo
-    g.start(); // inicia la ejecucion del juego
+
+    char option = 'X';
+    do {
+        std::cout <<  "Customize game (C) or use default values (D)?" << std::endl;
+        std::cin >> option;
+        if (option == 'C' || option == 'D') {
+            if (option == 'D') {
+                Manual g; // se crea una nueva instancia del juego para poder llamarlo
+                g.start(); // inicia la ejecucion del juego
+            } else if (option == 'C') {
+                /*
+                Agregar condiciones para preguntar los parámetros del juego
+                */
+            }
+            break;
+        }
+    } while(true);
+    
+
+
     return 0;
 }
